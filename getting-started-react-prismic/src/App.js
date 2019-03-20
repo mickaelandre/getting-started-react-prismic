614923c124a1f4b2f2e86e067f8b11dcd7e602a4
Mickael André <mickael.andre@efrei.net>
2019-03-20 20:55:13 +0100

    v1.0.0

--- getting-started-react-prismic/src/App.js
+++ getting-started-react-prismic/src/App.js
@@ -8,12 +8,14 @@ import {
 import Preview from './Preview';
 import Help from './Help';
 import NotFound from './NotFound';
+import Page from './Page';
 
 const App = (props) => (
   <Router>
     <Switch>
       <Redirect exact from="/" to="/help"/>
       <Route exact path="/help" component={Help} />
+      <Route exact path="/page/:uid" render={routeProps => <Page {...routeProps} prismicCtx={props.prismicCtx} />} />
       <Route exact path="/preview" render={routeProps => <Preview {...routeProps} prismicCtx={props.prismicCtx} />} />
       <Route component={NotFound} />
     </Switch>
--- getting-started-react-prismic/src/Page.js
+++ getting-started-react-prismic/src/Page.js
@@ -40,7 +40,20 @@ export default class Page extends React.Component {
     }
 
     render() {
-        // We will fill in this section in Step 5...
-        return null;
+        if (this.state.doc) {
+            return (
+                <div data-wio-id={this.state.doc.id}>
+                    {/* This is how to get an image into your template */}
+                    <img alt="cover" src={this.state.doc.data.image.url} />
+                    {/* This is how to insert a Rich Text field as plain text */}
+                    <h1>{PrismicReact.RichText.asText(this.state.doc.data.title)}</h1>
+                    {/* This is how to insert a Rich Text field into your template as html */}
+                    {PrismicReact.RichText.render(this.state.doc.data.description, this.props.prismicCtx.linkResolver)}
+                </div>
+            );
+        } else if (this.state.notFound) {
+            return <NotFound />;
+        }
+        return <h1>Loading</h1>;
     }
 }
