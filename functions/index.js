43635d87c8a831054c2463b8d303f7212d8fad6c
MickaelAndre <mickael.andre@efrei.net>
2019-03-21 16:38:57 +0100

    retrieving the test page v1

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,31 @@
 const functions = require('firebase-functions');
+//const fetch = require("node-fetch");
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
-    console.log("response " + request.method);
-    response.send("Hello World from Firebase!");
+exports.helloWorld = functions.https.onRequest(async (request, response) => {
+    console.log("my masterRef: " + request.body["masterRef"]);
+
+
+    /*
+    const param = {
+        method: 'GET',
+        mode: 'cors',
+        cache: 'default'
+    };
+
+    try{
+        const res = await fetch("https://getting-started-react-prismic.prismic.io/api/v2/documents/search?ref=" + "XJOk1hAAAOfGXeFa",param);
+        //const res = await fetch("https://getting-started-react-prismic.prismic.io/api/v2/documents/search?ref=" + request.body["masterRef"],param);
+        const json = await res.json();
+        console.log("json : "+ json);
+    }catch(e){
+        console.log("error: " + e);
+    }
+
+    */
+
+    response.send("cloud function executed !");
 });
